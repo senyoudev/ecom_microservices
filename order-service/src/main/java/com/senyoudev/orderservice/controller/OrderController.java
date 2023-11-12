8ff4a03a5f48387217d911b9167f1569af348676
senyoudev <younesmeskafe2018@gmail.com>
2023-11-12 17:45:39 +0100

    Circuit Breaker

--- order-service/src/main/java/com/senyoudev/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/senyoudev/orderservice/controller/OrderController.java
@@ -3,10 +3,15 @@ package com.senyoudev.orderservice.controller;
 
 import com.senyoudev.orderservice.dto.OrderRequest;
 import com.senyoudev.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -18,9 +23,16 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         log.info("Order placed for customer: {}", orderRequest.getOrderLineItemsDtos());
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
